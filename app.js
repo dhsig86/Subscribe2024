5cbda8fe424fb7c47479a63cc67a07b22dd69e53
dhsig86 <126081715+dhsig86@users.noreply.github.com>
2024-03-27 23:21:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const buyButtons = document.querySelectorAll('.buy-button');
-  
+
     buyButtons.forEach(button => {
-      button.addEventListener('click', (e) => {
-        e.preventDefault();
-        // Aqui você pode inserir a lógica para redirecionar para os links do Mercado Pago
-        // Por exemplo, se cada botão tiver um data attribute com a URL
-        const mercadoPagoLink = button.getAttribute('data-mercado-pago-link');
-        window.location.href = mercadoPagoLink; // Redireciona para o link do Mercado Pago
-      });
+        button.addEventListener('click', (e) => {
+            e.preventDefault();
+            const mercadoPagoLink = button.getAttribute('data-mercado-pago-link');
+            window.location.href = mercadoPagoLink; // Redireciona para o link do Mercado Pago
+        });
     });
-  });
-  
+
+    // Navegação suave para a barra de navegação móvel
+    document.querySelectorAll('.mobile-navbar a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const sectionToScrollTo = document.querySelector(this.getAttribute('href'));
+            sectionToScrollTo.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
